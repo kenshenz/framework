0132fdebc3fec2b579eb4271f03f725a5e1faad9
kenshen <kenshenchan@gmail.com>
2016-03-08 16:22:18 +0800

    add logback library

--- test/com/ksn/core/utils/DateTimeUtilsTest.java
+++ test/com/ksn/core/utils/DateTimeUtilsTest.java
@@ -3,36 +3,34 @@ package com.ksn.core.utils;
 import java.util.Date;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DateTimeUtilsTest {
+	Logger logger = LoggerFactory.getLogger(DateTimeUtilsTest.class);
 
 	@Test
 	public void test1() {
-		
 		String date2string = DateTimeUtils.date2string(null);
-		
-		System.out.println(date2string);
-		
+		logger.info(date2string);
 	}
 	
 	@Test
 	public void test2() {
 		Date string2date = DateTimeUtils.string2date("2016-04-07");
-		
-		System.out.println(string2date);
+		logger.info(string2date.toString());
 	}
 	
 	@Test
 	public void test3() {
-		System.out.println(DateTimeUtils.isAM(new Date()));
+		logger.info(""+DateTimeUtils.isAM(new Date()));
 	}
 	
 	@Test
 	public void test4() {
 		Date start = DateTimeUtils.string2date("2016-03-07");
 		Date end = DateTimeUtils.string2date("2016-04-07");
-		System.out.println(DateTimeUtils.getIntervalOfDay(end, start));
-		
+		logger.info(""+DateTimeUtils.getIntervalOfDay(end, start));
 	}
 
 }
