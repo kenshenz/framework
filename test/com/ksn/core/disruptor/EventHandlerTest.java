23fbc5e7a5422bd613d42c93dd366f596a7ae33f
kenshen <kenshenchan@gmail.com>
2016-06-12 16:25:32 +0800

    update

--- test/com/ksn/core/disruptor/EventHandlerTest.java
+++ test/com/ksn/core/disruptor/EventHandlerTest.java
@@ -3,19 +3,26 @@ package com.ksn.core.disruptor;
 import java.nio.ByteBuffer;
 import java.util.concurrent.Executors;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 
 public class EventHandlerTest {
+	
+	LongEventFactory factory;
+	
+	@Before
+	public void before() {
+		factory = new LongEventFactory();
+	}
 
 	@Test
+	@Ignore
 	public void test() throws InterruptedException {
-//		Executor executor = Executors.newCachedThreadPool();
-		
-		LongEventFactory factory = new LongEventFactory();
-		
 		Disruptor<LongEvent> disruptor = new Disruptor<LongEvent>(factory, 1024, Executors.defaultThreadFactory());
 		
 		disruptor.handleEventsWith(new LongEventHandler(), new LongEventHandler(), new LongEventHandler());
@@ -34,6 +41,42 @@ public class EventHandlerTest {
 //			Thread.sleep(1);
 		}
 		
+	}
+	
+	/**
+	 * 生产者必须等到所有消费者处理完才能往ringbuffer里面填
+	 */
+	@Test
+	public void test2() {
+		Disruptor<LongEvent> disruptor = new Disruptor<LongEvent>(factory, 8, Executors.defaultThreadFactory());
+		
+		disruptor.handleEventsWith(new EventHandler<LongEvent>() {
+
+			@Override
+			public void onEvent(LongEvent event, long sequence, boolean endOfBatch) throws Exception {
+				System.out.println("event: 1, sequence: " + sequence);
+			}
+		}, new EventHandler<LongEvent>() {
+
+			@Override
+			public void onEvent(LongEvent event, long sequence, boolean endOfBatch) throws Exception {
+				System.out.println("event: 2, sequence: " + sequence);
+				Thread.currentThread().sleep(500000);
+			}
+		});
+		
+		disruptor.start();
+		
+		RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
+		
+		LongEventProducer producer = new LongEventProducer(ringBuffer);
+		
+		ByteBuffer bb = ByteBuffer.allocate(8);
+		
+		for (long l = 0; l < 20; l++) {
+			bb.putLong(0, l);
+			producer.publish(bb);
+		}
 	}
 
 }
--- test/com/ksn/core/disruptor/WorkHandlerTest.java
+++ test/com/ksn/core/disruptor/WorkHandlerTest.java
@@ -1,14 +1,14 @@
 package com.ksn.core.disruptor;
 
-import static org.junit.Assert.fail;
-
 import java.nio.ByteBuffer;
 import java.util.concurrent.Executors;
 
 import org.junit.Test;
 
+import com.lmax.disruptor.BlockingWaitStrategy;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
 
 public class WorkHandlerTest {
 
@@ -16,20 +16,18 @@ public class WorkHandlerTest {
 	public void test() {
 		LongEventFactory factory = new LongEventFactory();
 		
-		Disruptor<LongEvent> disruptor = new Disruptor<LongEvent>(factory, 1024, Executors.defaultThreadFactory());
+		Disruptor<LongEvent> disruptor = new Disruptor<LongEvent>(factory, 1024, Executors.defaultThreadFactory(),
+				ProducerType.MULTI, new BlockingWaitStrategy());
 		
 		disruptor.handleEventsWithWorkerPool(new LongEventWorkHandler(), new LongEventWorkHandler());
 		
-		disruptor.start();
-		
-		
-		RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
+		RingBuffer<LongEvent> ringBuffer = disruptor.start();
 		
 		LongEventProducer producer = new LongEventProducer(ringBuffer);
 		
 		ByteBuffer bb = ByteBuffer.allocate(8);
 		
-		for (long l = 0; l < 20; l++) {
+		for (long l = 0; true; l++) {
 			bb.putLong(0, l);
 			producer.publish(bb);
 //			Thread.sleep(1);
